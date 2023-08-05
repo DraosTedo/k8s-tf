1ba90a264c8ebbd15e61995c85b544197b06cde4
Draos Tedo <teodorkostadinov@abv.bg>
2023-08-05 21:32:56 +0300

    Changing the  file

--- Terraform Code/Sg.tf
+++ Terraform Code/Sg.tf
@@ -8,10 +8,18 @@ resource "aws_instance" "demo-server" {
     instance_type = "t3.micro"
     key_name = "demo"
     security_groups = ["demo-sg"]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.demo-public-subnet-01.id
+    for_each = toset(["jenkins-master", "jenkins-worker", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
 }
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id = aws_vpc.demo-vpc.id
+
 
   ingress {
     description      = "SSH Access"
@@ -32,3 +40,65 @@ resource "aws_security_group" "demo-sg" {
     Name = "ssh-port"
   }
 }
+
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "demo-vpc"
+  }
+  
+}
+
+resource "aws_subnet" "demo-public-subnet-01" {
+  vpc_id = aws_vpc.demo-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "eu-west-2"
+  tags = {
+    Name = "demo-public-subnet-01"
+
+  }
+  
+}
+
+resource "aws_subnet" "demo-public-subnet-02" {
+  vpc_id = aws_vpc.demo-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "eu-west-3"
+  tags = {
+    Name = "demo-public-subnet-02"
+    
+  }
+  
+}
+
+resource "aws_internet_gateway" "demo-igw" {
+  vpc_id = aws_vpc.demo-vpc.id
+  tags = {
+    Name = "demo-igw"
+  }
+  
+}
+
+resource "aws_route_table" "demo-public-rt" {
+  vpc_id = aws_vpc.demo-vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo-igw.id
+  }
+  
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-01" {
+  subnet_id = aws_subnet.demo-public-subnet-01.id
+  route_table_id = aws_route_table.demo-public-rt.id
+
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-02" {
+  subnet_id = aws_subnet.demo-public-subnet-02.id
+  route_table_id = aws_route_table.demo-public-rt.id
+  
+}
+
