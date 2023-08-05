f0ace4f6ae8f13269e1741ed88f5eaf7ae1f62e2
Draos Tedo <teodorkostadinov@abv.bg>
2023-08-05 17:07:48 +0300

    adding the changed  file

--- Terraform Code/ec2.tf
+++ Terraform Code/ec2.tf
@@ -1,36 +1,9 @@
 provider "aws" {
   region = "eu-west-2"
-  
 }
 
 resource "aws_instance" "demo-server" {
     ami = "ami-05b5a865c3579bbc4"
     instance_type = "t3.micro"
     key_name = "demo"
-}
-resource "aws_security_group" "SSH Access" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
 }
